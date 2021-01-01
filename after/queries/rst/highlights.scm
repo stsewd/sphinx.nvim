1edc1bc9c5c75e83c2c189b084e51517c8d36a5e
Santos Gallegos <stsewd@protonmail.com>
2021-01-01 15:00:03 -0500

    Add queries for domains

--- after/queries/rst/highlights.scm
+++ after/queries/rst/highlights.scm
@@ -19,3 +19,83 @@
  (#eq? @constant.builtin "|version|"))
 ((substitution_reference) @constant.builtin
  (#eq? @constant.builtin "|today|"))
+
+; Python domain
+; https://www.sphinx-doc.org/en/master/usage/restructuredtext/domains.html#the-python-domain
+((directive
+   name: (type) @function.builtin)
+ (#match?
+  @function.builtin
+  "^py:(module|currentmodule|function|data|exception|class|attribute|method|staticmethod|classmethod|decorator|decoratormethod)$"))
+
+((role) @function.builtin
+ (#match?
+  @function.builtin
+  "^:py:(mod|func|data|const|class|meth|attr|exc|obj):$"))
+
+; C Domain
+; https://www.sphinx-doc.org/en/master/usage/restructuredtext/domains.html#the-c-domain
+((directive
+   name: (type) @function.builtin)
+ (#match?
+  @function.builtin
+  "^c:(member|var|function|macro|struct|union|enum|enumerator|type|alias|namespace|namespace-push|namespace-pop)$"))
+
+((role) @function.builtin
+ (#match?
+  @function.builtin
+  "^:c:(member|data|var|func|macro|struct|union|enum|enumerator|type|expr|texpr):$"))
+
+; Cpp domain
+; https://www.sphinx-doc.org/en/master/usage/restructuredtext/domains.html#cpp-domain
+((directive
+   name: (type) @function.builtin)
+ (#match?
+  @function.builtin
+  "^cpp:(class|struct|function|member|var|type|enum|enum-struct|enum-class|enumerator|union|concept|alias|namespace|namespace-push|namespace-pop|)$"))
+
+((role) @function.builtin
+ (#match?
+  @function.builtin
+  "^:cpp:(expr|texpr|any|class|struct|func|member|var|type|concept|enum|enumerator):$"))
+
+; Standard domain
+; https://www.sphinx-doc.org/en/master/usage/restructuredtext/domains.html#the-standard-domain
+((directive
+   name: (type) @function.builtin)
+ (#match?
+  @function.builtin
+  "^(default-domain|option|envar|program|describe|object|)$"))
+
+; JavaScript domain
+; https://www.sphinx-doc.org/en/master/usage/restructuredtext/domains.html#the-javascript-domain
+((directive
+   name: (type) @function.builtin)
+ (#match?
+  @function.builtin
+  "^js:(module|function|method|class|data|attribute)$"))
+
+((role) @function.builtin
+ (#match?
+  @function.builtin
+  "^:js:(mod|func|meth|class|data|attr):$"))
+
+; reStructuredtext domain
+; https://www.sphinx-doc.org/en/master/usage/restructuredtext/domains.html#the-restructuredtext-domain
+((directive
+   name: (type) @function.builtin)
+ (#match?
+  @function.builtin
+  "^rst:(directive|directive:option|role)$"))
+
+((role) @function.builtin
+ (#match?
+  @function.builtin
+  "^:rst:(dir|role):$"))
+
+; Math domain
+; https://www.sphinx-doc.org/en/master/usage/restructuredtext/domains.html#the-math-domain
+((role) @function.builtin
+ (#match?
+  @function.builtin
+  "^:math:(numref):$"))
