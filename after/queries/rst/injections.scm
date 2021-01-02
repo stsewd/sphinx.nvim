c5f45801e2bca335f10f46704207be39c4c4e721
Santos Gallegos <stsewd@protonmail.com>
2021-01-02 17:21:44 -0500

    Update injection queries

--- after/queries/rst/injections.scm
+++ after/queries/rst/injections.scm
@@ -2,20 +2,28 @@
 
 ((directive
    name: (type) @_type
-   body: (body) @rst)
+   body: (body (content) @rst))
  (#match?
   @_type
-  "^(toctree)|(versionadded|versionchanged|deprecated|seealso|centered|hlist)|(literalinclude)|(glossary)|(sectionauthor|codeauthor)|(index)|(only)|(tabularcolumns)|(productionlist)$"))
+  "^(toctree)|(versionadded|versionchanged|deprecated|seealso|centered|hlist)|(glossary)|(index)|(productionlist)$"))
 
-;; Special directives
-;; TODO: using @language and @content on the same capture raises an error.
-;; ((directive
-;;    name: (type) @_type
-;;    body: (body) @language @content)
-;;  (#eq? @_type "highlight"))
+((directive
+   name: (type) @_type
+   body: (body (arguments) @rst))
+ (#match? @_type "^(sectionauthor|codeauthor)$"))
 
-;; TODO: using @language and @content on the same capture raises an error.
-;; ((directive
-;;    name: (type) @_type
-;;    body: (body) @language @content)
-;;  (#eq? @_type "code-block"))
+;; Code highlight directives
+((directive
+   name: (type) @_type
+   body: (body (arguments) @language (content) @content))
+ (#match? @_type "^(highlight|code-block)$"))
+
+;; Most directives from domains accept nested content
+((directive
+   name: (type) @_type
+   body: (body (content) @rst))
+ (#match? @_type "^(py|c|cpp|js|rst):$"))
+((directive
+   name: (type) @_type
+   body: (body (content) @rst))
+ (#match? @_type "^(default-domain|option|envar|program|describe|object)$"))
